18e49596d8688398819fb448251b347e24a8708d
Line Svensen <144799112+LineSvensen@users.noreply.github.com>
2026-04-15 14:51:33 +0200

    contact page started on

--- src/Pages/Kontakt/index.jsx
+++ src/Pages/Kontakt/index.jsx
@@ -1,8 +1,8 @@
 export default function Kontakt() {
   return (
     <div className="max-w-5xl mx-auto p-8">
-      <h1 className="text-3xl font-bold text-gray-800 mb-4">kontakt</h1>
-      <p className="text-gray-700">kontakt text kommer</p>
+      <h1 className="text-3xl font-bold text-gray-800 mb-4">Kontakt Marit</h1>
+      <p className="text-gray-700">Her kan du kontakte Marit for å bestille.</p>
     </div>
   );
 }

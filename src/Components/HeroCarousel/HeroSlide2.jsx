e76053ec6fdd4ad9d3e986f86499ad007aea9931
Line Svensen <144799112+LineSvensen@users.noreply.github.com>
2026-04-23 22:30:42 +0200

    slide two new img and fixes

--- src/Components/HeroCarousel/HeroSlide2.jsx
+++ src/Components/HeroCarousel/HeroSlide2.jsx
@@ -1,19 +1,19 @@
 import { Link } from "react-router-dom";
 import deal from "../../assets/deal.png";
 import blueDots from "../../assets/scratch.png";
-import allBooksOne from "../../assets/abone.png";
+import allBooksOne from "../../assets/abfour.png";
 
 export default function HeroSlide2() {
   return (
     <div
-      className="relative flex flex-col lg:flex-row items-center justify-center h-full w-full px-6 lg:px-16 py-0 sm:py-10 rounded bg-cover bg-center bg-no-repeat"
+      className="relative flex flex-col sm:flex-row items-center justify-center h-full w-full px-6 lg:px-16 py-0 sm:py-10 rounded bg-cover bg-center bg-no-repeat"
       style={{ backgroundImage: `url(${blueDots})` }}
     >
       {/* overlay */}
-      <div className="absolute inset-0 bg-linear-to-r from-red-400/30   to-white/80"></div>
+      <div className="absolute inset-0  bg-linear-to-b from-white/99  to-white/10 sm:bg-linear-to-r sm:from-white/99  sm:to-white/10"></div>
 
       {/* text content */}
-      <div className="relative backdrop-blur-md bg-pink-100 lg:h-[300px] shadow-lg z-10 px-4 sm:px-8 py-4 sm:p-8  lg:pl-8 lg:pr-8 rounded-xl flex flex-col  justify-center items-center lg:items-start text-center lg:text-left w-full lg:w-1/2 max-w-lg gap-3 lg:gap-5">
+      <div className="relative  lg:h-[300px]  z-10 px-4 sm:px-8 py-4 sm:p-8  lg:pl-8 lg:pr-8 rounded-xl flex flex-col  justify-center items-center lg:items-start text-center lg:text-left w-full lg:w-1/2 max-w-lg gap-3 lg:gap-5">
         <h2 className="text-2xl flex flex-row justify-center items-center pb-3 md:text-4xl font-bold text-black font-poppins ">
           Skaff deg alle bøkene
         </h2>
@@ -31,11 +31,11 @@ export default function HeroSlide2() {
       </div>
 
       {/* image */}
-      <div className="relative z-10 flex justify-center lg:justify-end items-center pl-8 sm:pl-12 lg:pl-0 w-full lg:w-1/2 mt-8 lg:mt-0">
+      <div className="relative z-10 flex justify-center lg:justify-end items-center  sm:pl-12 lg:pl-0 w-full lg:w-1/2 mt-2 sm:mt-8 lg:mt-0">
         <img
           src={allBooksOne}
           alt="Bøker og gavepakke"
-          className="w-[280px] sm:w-[360px] md:w-[400px] lg:w-[420px] object-contain drop-shadow-lg hover:scale-105 transition-transform duration-500"
+          className="w-[280px] sm:w-[360px] md:w-[300px] lg:w-[500px] object-contain drop-shadow-lg flex items-center justify-center  transition-transform duration-500"
         />
       </div>
     </div>

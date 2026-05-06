4179bfbb55f34ad4748c31f6b0c3a0762fcef9d8
Line Svensen <144799112+LineSvensen@users.noreply.github.com>
2026-05-06 23:01:48 +0200

    scroll to top auto

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import ScrollToTop from "./Components/ScrollToTop";
 import Layout from "./Layout";
 import Hjem from "./Pages/Hjem";
 import OmMeg from "./Pages/OmMeg";
@@ -10,17 +11,20 @@ import Presse from "./Pages/Presse";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Hjem />} />
-        <Route path="om-meg" element={<OmMeg />} />
-        <Route path="diktboker" element={<Diktboker />} />
-        <Route path="diktboker/:slug" element={<EnkeltProdukt />} />
-        <Route path="kontakt" element={<Kontakt />} />
-        <Route path="kundeomtaler" element={<Kundeomtaler />} />
-        <Route path="presse" element={<Presse />} />
-      </Route>
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Hjem />} />
+          <Route path="om-meg" element={<OmMeg />} />
+          <Route path="diktboker" element={<Diktboker />} />
+          <Route path="diktboker/:slug" element={<EnkeltProdukt />} />
+          <Route path="kontakt" element={<Kontakt />} />
+          <Route path="kundeomtaler" element={<Kundeomtaler />} />
+          <Route path="presse" element={<Presse />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
